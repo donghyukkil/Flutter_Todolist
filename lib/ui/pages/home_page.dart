625ed255938460ace0df26896c04175ef4dfe67b
donghyukkil <asterism90@gmail.com>
2024-03-16 13:42:54 +0900

    feat: 작업 상태 별 탭 추가

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'package:todolist/core/viewmodels/task_viewmodel.dart';
 import 'package:todolist/utils/dialog_utils.dart';
+import 'package:todolist/data/models/task.dart';
 
 class HomePage extends StatelessWidget {
   final TaskViewModel taskViewModel = Get.find();
@@ -12,75 +12,93 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('To-do List'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('To-do List'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.list), text: 'In Progress'),
+              Tab(icon: Icon(Icons.done_all), text: 'Completed'),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: textEditingController,
+                decoration: InputDecoration(
+                    labelText: 'Enter your task',
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () {
+                        taskViewModel
+                            .addTaskIfNotEmpty(textEditingController.text);
+                        textEditingController.clear();
+                      },
+                    )),
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Obx(() => _buildTaskList(taskViewModel.tasks
+                      .where((task) => !task.isDone.value)
+                      .toList())),
+                  Obx(() => _buildTaskList(taskViewModel.tasks
+                      .where((task) => task.isDone.value)
+                      .toList())),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: textEditingController,
-              decoration: InputDecoration(
-                  labelText: 'Enter your task',
-                  suffixIcon: IconButton(
-                    icon: const Icon(Icons.add),
-                    onPressed: () {
-                      taskViewModel
-                          .addTaskIfNotEmpty(textEditingController.text);
-                      textEditingController.clear();
-                    },
-                  )),
+    );
+  }
+
+  Widget _buildTaskList(List<Task> tasks) {
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        final task = tasks[index];
+        return Card(
+          margin: const EdgeInsets.all(8.0),
+          child: ListTile(
+            title: Text(
+              task.title.value,
+              style: task.isDone.value
+                  ? const TextStyle(decoration: TextDecoration.lineThrough)
+                  : null,
+            ),
+            leading: Checkbox(
+              value: task.isDone.value,
+              onChanged: (bool? newValue) {
+                if (newValue != null) {
+                  taskViewModel.toggleTaskStatus(task.id);
+                }
+              },
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () => DialogUtils.showEditTaskDialog(
+                      context, task, taskViewModel),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () => taskViewModel.deleteTask(task.id),
+                ),
+              ],
             ),
           ),
-          Expanded(
-            child: Obx(() => ListView.builder(
-                  itemCount: taskViewModel.tasks.length,
-                  itemBuilder: (context, index) {
-                    final task = taskViewModel.tasks[index];
-
-                    return Card(
-                      margin: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        title: Text(task.title.value,
-                            style: task.isDone.value
-                                ? const TextStyle(
-                                    decoration: TextDecoration.lineThrough)
-                                : null),
-                        leading: Checkbox(
-                          value: task.isDone.value,
-                          onChanged: (bool? newValue) {
-                            if (newValue != null) {
-                              taskViewModel.toggleTaskStatus(task.id);
-                            }
-                          },
-                        ),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              icon: const Icon(Icons.edit),
-                              onPressed: () {
-                                DialogUtils.showEditTaskDialog(
-                                    context, task, taskViewModel);
-                              },
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.delete),
-                              onPressed: () {
-                                taskViewModel.deleteTask(task.id);
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                )),
-          )
-        ],
-      ),
+        );
+      },
     );
   }
 }
